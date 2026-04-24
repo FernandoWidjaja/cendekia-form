604285489d39eae98e0d76511e70075056b7a993
FernandoWidjaja <widj47@gmail.com>
2026-04-24 11:19:10 +0700

    fix: user Vercel KV env vars for redis connection

--- src/lib/redis-client.js
+++ src/lib/redis-client.js
@@ -12,8 +12,8 @@
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
-    url: process.env.REDIS_REST_URL || process.env.UPSTASH_REDIS_REST_URL || "",
-    token: process.env.REDIS_REST_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN || "",
+    url: process.env.KV_REST_API_URL || process.env.REDIS_REST_URL || process.env.UPSTASH_REDIS_REST_URL || "",
+    token: process.env.KV_REST_API_TOKEN || process.env.REDIS_REST_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN || "",
 });
 
 export default redis;
